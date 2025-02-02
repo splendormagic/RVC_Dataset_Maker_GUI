6bc9536ff4bc42478b7c908634b504ef68309e87
splendormagic <splendor.magic@gmail.com>
2025-02-02 16:27:03 +0200

    Create install.py

---
+++ install.py
@@ -0,0 +1,20 @@
+import sys
+import subprocess
+
+def install_dependencies():
+    # List of required packages
+    packages = ['pydub', 'gradio', 'demucs', 'ffmpeg-python']
+    
+    # Check for each package, install if not present
+    for package in packages:
+        if package not in sys.modules:
+            subprocess.run(['pip', 'install', package])
+
+    # Install ffmpeg without triggering runtime restart
+    subprocess.run(['apt-get', 'install', '-y', 'ffmpeg'])
+
+install_dependencies()
+
+from IPython.display import clear_output
+clear_output()
+print("Installed")
